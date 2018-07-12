9e0d1a3ed6c3172970e2659c881e063c770cf856
Cyriel Basilio <cyriel@CYRIELs-iMac.local>
2018-07-12 12:55:14 +0800

    Add form

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -5,57 +5,79 @@ import { Select, Card, Form, Input, Button } from "antd";
 const Option = Select.Option;
 const FormItem = Form.Item;
 const authorQuery = gql`
-  {
-    authors {
-      id
-      name
-    }
-  }
+	{
+		authors {
+			id
+			name
+		}
+	}
 `;
 
 class AddBook extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <Card>
-          <Form onSubmit={this.handleSubmit}>
-            <FormItem>
-              <Input
-                name="name"
-                placeholder="Name"
-                onChange={this.handleChange}
-              />
-            </FormItem>
-            <FormItem>
-              <Input
-                name="genre"
-                placeholder="Genre"
-                onChange={this.handleChange}
-              />
-            </FormItem>
-            <FormItem>
-              <Select
-                style={{ width: 120 }}
-                onChange={val => console.log({ val })}
-              >
-                {this.props.data.loading
-                  ? console.log("fail")
-                  : this.props.data.authors.map(author => (
-                      <Option value={author.name} key={author.id}>
-                        {author.name}
-                      </Option>
-                    ))}
-              </Select>
-            </FormItem>
-            <Button htmlType="submit" type="primary">
-              LOG IN
-            </Button>
-          </Form>
-        </Card>
-      </div>
-    );
-  }
+	state = {
+		name: "",
+		genre: "",
+		authorId: ""
+	};
+	handleSubmit = e => {
+		e.preventDefault();
+		console.log(this.state);
+	};
+	handleChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+	render() {
+		console.log(this.props);
+		return (
+			<div>
+				<Card>
+					<Form onSubmit={this.handleSubmit}>
+						<FormItem>
+							<Input
+								name="name"
+								placeholder="Name"
+								value={this.state.name}
+								onChange={this.handleChange}
+							/>
+						</FormItem>
+						<FormItem>
+							<Input
+								name="genre"
+								placeholder="Genre"
+								value={this.state.genre}
+								onChange={this.handleChange}
+							/>
+						</FormItem>
+						<FormItem>
+							<Select
+								style={{ width: 120 }}
+								onChange={val =>
+									this.setState({ authorId: val })
+								}
+							>
+								{this.props.data.loading
+									? console.log("fail")
+									: this.props.data.authors.map(author => (
+											<Option
+												value={author.id}
+												name={author.name}
+												key={author.id}
+											>
+												{author.name}
+											</Option>
+									  ))}
+							</Select>
+						</FormItem>
+						<Button htmlType="submit" type="primary">
+							ADD
+						</Button>
+					</Form>
+				</Card>
+			</div>
+		);
+	}
 }
 
 export default graphql(authorQuery)(AddBook);
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
+import Spinner from "react-spinkit";
 
 const bookQuery = gql`
-  {
-    books {
-      id
-      name
-      genre
-    }
-  }
+	{
+		books {
+			id
+			name
+			genre
+		}
+	}
 `;
 
 class BookList extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.props.data.loading
-            ? console.log("fail")
-            : this.props.data.books.map(book => (
-                <li key={book.id}>{book.name}</li>
-              ))}
-        </ul>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<ul>
+					{this.props.data.loading ? (
+						<Spinner name="ball-scale-multiple" />
+					) : (
+						this.props.data.books.map(book => (
+							<li key={book.id}>{book.name}</li>
+						))
+					)}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default graphql(bookQuery)(BookList);
